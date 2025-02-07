29d065234fe483938138d22300ed7fa9c23e8f63
SonaliITp <104667397+SonaliITp@users.noreply.github.com>
2025-02-07 22:31:29 +0530

    Update Help.jsx

--- src/Help.jsx
+++ src/Help.jsx
@@ -5,6 +5,8 @@ function Help() {
     <div className='container m-3 m-auto'>
         <h3 className='text-secondary'>This is Help Component</h3>
 
+      <p1>This is para one</p1>
+
     </div>
   )
 }
